8ee4b5e7f12664c5640a05058eda04c05af69a29
Filip Mazurek <fjm7@duke.edu>
2016-09-23 17:24:17 -0400

    Removed normalization. No need since input is unsigned.

--- Beat_Detector.py
+++ Beat_Detector.py
@@ -15,23 +15,23 @@ class Beat_Detector:
 
     def single_array_hr(self, data_array):
 
-        normalized_data_array = self.normalize_array(data_array)
+        # normalized_data_array = self.normalize_array(data_array)
 
-        num_beats = self.get_num_beats(normalized_data_array)
+        num_beats = self.get_num_beats(data_array)
 
         bpm = num_beats / (60 / self.update_time_seconds)  # 60 seconds in a minute
 
         return bpm
 
-    def normalize_array(self, some_array):
-        normalized_array = (some_array - np.min(some_array)) / np.ptp(some_array)
-        return normalized_array
+    # def normalize_array(self, some_array):
+    #     normalized_array = (some_array - np.min(some_array)) / np.ptp(some_array)
+    #     return normalized_array
 
     def get_num_beats(self, some_normalized_array):
         num_beats = 0
         threshold = (max(some_normalized_array) - np.mean(some_normalized_array)) / 2
 
-        for i in range(0, some_normalized_array.size):
+        for i in range(0, some_normalized_array.size - 1):
             if (some_normalized_array[i] < threshold) and (some_normalized_array[i+1] > threshold):
                 num_beats += 1
 
@@ -42,6 +42,6 @@ class Beat_Detector:
             return hr_one
 
         else:
-            return np.mean(hr_one, hr_two)
+            return (hr_one + hr_two) / 2
 
         # TODO: make actual good reconciliation. Not this lazy implementation.
--- Test_Beat_Detector.py
+++ Test_Beat_Detector.py
@@ -1,2 +1,16 @@
 from Beat_Detector import Beat_Detector
 
+update_time_seconds = 20
+
+# def test_normalize_array():
+#     beat_detector = Beat_Detector(update_time_seconds)
+#     sample_array = [0, 2, 24, 2, 15]
+#
+#     normalized_array = beat_detector.normalize_array(sample_array)
+#     print(normalized_array)
+#     is_normal = True
+#     for num in normalized_array:
+#         if (num > 1) or (num < 0):
+#             is_normal = False
+#
+#     assert is_normal

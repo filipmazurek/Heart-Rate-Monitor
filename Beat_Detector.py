Adds tests, fixes bpm calculation.

Beat_Detector.py
@@ -19,7 +19,7 @@ class Beat_Detector:
 
         num_beats = self.get_num_beats(data_array)
 
-        bpm = num_beats / (60 / self.update_time_seconds)  # 60 seconds in a minute
+        bpm = num_beats / (self.update_time_seconds / 60)  # 60 seconds in a minute
 
         return bpm
 
@@ -27,12 +27,12 @@ class Beat_Detector:
     #     normalized_array = (some_array - np.min(some_array)) / np.ptp(some_array)
     #     return normalized_array
 
-    def get_num_beats(self, some_normalized_array):
+    def get_num_beats(self, some_array):
         num_beats = 0
-        threshold = (max(some_normalized_array) - np.mean(some_normalized_array)) / 2
+        threshold = (max(some_array) - np.mean(some_array)) / 2
 
-        for i in range(0, some_normalized_array.size - 1):
-            if (some_normalized_array[i] < threshold) and (some_normalized_array[i+1] > threshold):
+        for i in range(0, len(some_array) - 1):
+            if (some_array[i] < threshold) and (some_array[i+1] > threshold):
                 num_beats += 1
 
         return num_beats
cc116102efe6507222e870665b4c163b02430a6f
Filip Mazurek <fjm7@duke.edu>
2016-09-23 23:00:12 -0400

    Adds the necessary Information_Passer class.

---
+++ Information_Passer.py
@@ -0,0 +1,35 @@
+class InformationPasserClass:
+    def __init__(self, inst_hr, one_min_hr, five_min_hr):
+        self.inst_hr = inst_hr
+        self.one_min_hr = one_min_hr
+        self.five_min_hr = five_min_hr
+        self.ten_min_log = None
+        self.bradycardia_alarm = False
+        self.tachycardia_alarm = False
+
+    def add_ten_min_log(self, someArray):
+        self.ten_min_log = someArray
+
+    def set_bradycardia_alarm(self):
+        self.bradycardia_alarm = True
+
+    def set_tachycardia_alarm(self):
+        self.tachycardia_alarm = True
+
+    def get_inst_hr(self):
+        return self.inst_hr
+
+    def get_one_min_hr(self):
+        return self.one_min_hr
+
+    def get_five_min_hr(self):
+        return self.five_min_hr
+
+    def get_ten_min_log(self):
+        return self.ten_min_log
+
+    def get_tachycardia_alarm(self):
+        return self.tachycardia_alarm
+
+    def get_bradycardia_alarm(self):
+        return self.bradycardia_alarm
